96b5bdd02c4a2946bcb9256853fc4191f56f8e18
Saeed Amiri <samiri@uni-bremen.de>
2023-08-24 13:19:14 +0200

    integeration

--- codes/dlvo.py
+++ codes/dlvo.py
@@ -5,13 +5,17 @@ Modeling DLVO
 
 import logger
 import read_param
-import system_initialization
 import force_copmutation
+import system_initialization
+import integrator
 
 if __name__ == "__main__":
     LOG = logger.setup_logger(log_name='dlvo.log')
     parameter = read_param.ReadParam(log=LOG)
     system = \
         system_initialization.TwoDSystem(parameter.parameters_dict, log=LOG)
-    forces = \
+    dlvo = \
         force_copmutation.DLVO(parameter.parameters_dict, system.particles)
+    integrator.ParticleIntegrator(parameter.parameters_dict,
+                                  system.particles,
+                                  dlvo.forces)
