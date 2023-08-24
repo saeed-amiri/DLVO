390423d91f6fc48c25d2a1b1267106925217de3a
Saeed Amiri <samiri@uni-bremen.de>
2023-08-24 14:27:30 +0200

    update; split methods

--- codes/integrator.py
+++ codes/integrator.py
@@ -4,15 +4,10 @@ from system_initialization import Particle, DisplaySystem
 
 
 class ParticleIntegrator:
-    """
-    Class to handle the integration of the equations of motion.
+    """Class to handle the integration of the equations of motion."""
 
-    Attributes:
-        dt (float): Time step for integration.
-        mass (float): Mass of the particles.
-    """
     def __init__(self,
-                 params: dict[str, float],  # All the parameters
+                 params: dict[str, float],
                  particles: "list[Particle]",
                  forces: np.ndarray
                  ) -> None:
@@ -21,38 +16,68 @@ class ParticleIntegrator:
         self.update_system(params, particles, forces)
 
     def update_system(self,
-                      params: dict[str, float],  # All the parameters
+                      params: dict[str, float],
                       particles: list["Particle"],
                       forces: np.ndarray
                       ) -> None:
         """Update system of particles using Velocity Verlet integration."""
         for t_step in range(int(params['total_steps'])):
-            new_pos = []
-            new_vels = []
-            hlf_vels = []
-            new_structre = []
-            for item, particle in enumerate(particles):
-                v_half_dt, new_position = \
-                    self.velocity_verlet(particle, forces[item], params)
-                hlf_vels.append(v_half_dt)
-                # Update particle position
-                new_pos.append(
-                    Particle((new_position[0], new_position[1]), (0, 0)))
-
-            dlvo = force_copmutation.DLVO(params, particles=new_pos)
-            for item, vel in enumerate(hlf_vels):
-                # Update velocity with the second half of Verlet integration
-                new_vels.append(
-                    vel +
-                    0.5 * self.delta_t *
-                    np.array(dlvo.forces[item]) / self.mass)
-
-            for pos, vel in zip(new_pos, new_vels):
-                new_structre.append(Particle(pos.position, (vel[0], vel[1])))
-            DisplaySystem(params, new_structre, out_label=f'frame{t_step}')
-            dlvo = force_copmutation.DLVO(params, new_structre)
-            particles = new_structre
+            hlf_vels, new_pos = \
+                self._compute_half_velocities_and_positions(
+                    particles, forces, params)
+            new_vels = self._compute_new_velocities(params, new_pos, hlf_vels)
+            new_structure = self._update_particle_structure(new_pos, new_vels)
+            DisplaySystem(params, new_structure, out_label=f'frame{t_step}')
+            dlvo = force_copmutation.DLVO(params, new_structure)
+            particles = new_structure
             forces = dlvo.forces.copy()
+
+    def _compute_half_velocities_and_positions(self,
+                                               particles: list["Particle"],
+                                               forces: np.ndarray,
+                                               params: dict[str, float]
+                                               ) -> tuple[list[np.ndarray],
+                                                          list["Particle"]
+                                                          ]:
+        new_pos: list["Particle"] = []
+        hlf_vels: list[np.ndarray] = []
+        for item, particle in enumerate(particles):
+            v_half_dt, new_position = \
+                self.velocity_verlet(particle, forces[item], params)
+            hlf_vels.append(v_half_dt)
+            new_pos.append(
+                Particle((new_position[0], new_position[1]), (0, 0)))
+        return hlf_vels, new_pos
+
+    def _compute_new_velocities(self,
+                                params: dict[str, float],
+                                new_pos: list["Particle"],
+                                hlf_vels: list[np.ndarray]
+                                ) -> list[np.ndarray]:
+        dlvo = force_copmutation.DLVO(params, particles=new_pos)
+        return self._update_velocity(dlvo.forces, hlf_vels)
+
+    def _update_particle_structure(self,
+                                   new_pos: list["Particle"],
+                                   new_vels: list[np.ndarray]
+                                   ) -> list["Particle"]:
+        new_structure = []
+        for pos, vel in zip(new_pos, new_vels):
+            new_structure.append(Particle(pos.position, (vel[0], vel[1])))
+        return new_structure
+
+    def _update_velocity(self,
+                         forces: np.ndarray,
+                         hlf_vels: list[np.ndarray]
+                         ) -> list[np.ndarray]:
+        """update velocity based on the updated forces"""
+        new_vels: list[np.ndarray] = []
+        for item, vel in enumerate(hlf_vels):
+            new_vels.append(
+                vel +
+                0.5 * self.delta_t *
+                np.array(forces[item]) / self.mass)
+        return new_vels
 
     def velocity_verlet(self,
                         particle: "Particle",
@@ -60,23 +85,28 @@ class ParticleIntegrator:
                         params: dict[str, float]
                         ) -> tuple[np.ndarray, list[float]]:
         """Velocity Verlet integrator."""
-        # Update velocity by half a time step
         v_half_dt = (
             np.array(particle.velocity) +
             0.5 * self.delta_t * np.array(force) / self.mass
         )
 
-        # Update particle position
+        new_position = self._compute_new_position(particle, v_half_dt, params)
+        return v_half_dt, new_position.tolist()
+
+    def _compute_new_position(self,
+                              particle: "Particle",
+                              v_half_dt: np.ndarray,
+                              params: dict[str, float]
+                              ) -> np.ndarray:
+        """
+        Compute the new position of a particle considering boundary conditions.
+        """
         new_position = np.array(particle.position) + self.delta_t * v_half_dt
         for i, axis in enumerate(['width', 'height']):
-            if new_position[i] - params['particle_radius'] < 0:
-                while new_position[i] - params['particle_radius'] < 0:
+            while (new_position[i] - params['particle_radius'] < 0 or
+                   new_position[i] + params['particle_radius'] > params[axis]):
+                if new_position[i] - params['particle_radius'] < 0:
                     new_position[i] += params[axis]
-            if new_position[i] + params['particle_radius'] > params[axis]:
-                while (
-                    new_position[i] + params['particle_radius'] > params[axis]
-                      ):
+                if new_position[i] + params['particle_radius'] > params[axis]:
                     new_position[i] -= params[axis]
-
-        # Return half-updated velocity and new position
-        return v_half_dt, new_position.tolist()
+        return new_position
